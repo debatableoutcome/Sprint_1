43bc314c9bd6a7fa5d1aed455eddee395e835607
m.verma <maria.verma.webdev@gmail.com>
2025-12-15 10:39:44 +0300

    fix after review: corrected attribute use

--- task_2.py
+++ task_2.py
@@ -4,13 +4,15 @@ class Tester:
         self.name = name
         self.deadline = deadline
 
-    def work_hard(self, deadline):
-        if deadline:
+    def work_hard(self):
+        if self.deadline:
             print(self.name, 'Что ж, ещё часок поработаю!')
         else:
             print(self.name, 'Можно отдыхать')
 
-tester_1 = Tester(name='tester_1')
-tester_1.work_hard(deadline=False)  # 'tester_1 Можно отдыхать'
-tester_2 = Tester(name='tester_2')
-tester_2.work_hard(deadline=True)   # 'tester_2 Что ж, ещё часок поработаю!'
+
+tester_1 = Tester(name='tester_1', deadline=False)
+tester_1.work_hard()   # tester_1 Можно отдыхать
+
+tester_2 = Tester(name='tester_2', deadline=True)
+tester_2.work_hard()   # tester_2 Что ж, ещё часок поработаю!
